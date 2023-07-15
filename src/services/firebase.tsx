9210a4dd0d51a0e2cd5853e8e6211810d1a5b8e9
Rohit Rai <raistudying@gmail.com>
2023-07-15 17:05:53 +0530

    Add Firebase config

---
+++ src/services/firebase.tsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMf0nNwjinPKLdDbDNnLLSm8gWehbYQBU",
+  authDomain: "photoshare-1f8e0.firebaseapp.com",
+  projectId: "photoshare-1f8e0",
+  storageBucket: "photoshare-1f8e0.appspot.com",
+  messagingSenderId: "189201255116",
+  appId: "1:189201255116:web:ec0101ae06adf7a8619069",
+  measurementId: "G-9YQYB2H6RN",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
