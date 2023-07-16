4f214b22093262b4ffcfdc64416baab170305067
Rohit Rai <raistudying@gmail.com>
2023-07-16 12:14:21 +0530

    Implement authenticated router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
+import { RouteObject, createBrowserRouter } from "react-router-dom";
 import SignIn from "../components/sign_in";
+import Home from "../components/home";
 
-const signIn = {
+const signIn: RouteObject = {
   path: "/",
   element: <SignIn />,
 };
 
+const home: RouteObject = {
+  path: "/",
+  element: <Home />,
+};
+
 export const router = createBrowserRouter([signIn]);
+
+export const authenticatedRouter = createBrowserRouter([home]);
--- src/services/authenticate.tsx
+++ src/services/authenticate.tsx
@@ -1,4 +1,9 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
 import app from "./firebase";
 
 const provider = new GoogleAuthProvider();
@@ -12,4 +17,19 @@ export const signIn = async () => {
     .catch((error) => {
       console.log("Error while signing in user: ", error);
     });
+};
+
+export const checkUserSignedIn = (
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
+  setIsUserSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setIsUserSignedIn(true);
+    } else {
+      console.log("User is signed out");
+      setIsUserSignedIn(false);
+    }
+    setIsLoading(false);
+  });
 };
