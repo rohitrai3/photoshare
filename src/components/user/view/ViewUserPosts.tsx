fcc5efc88ce4492d5288dfca862a2c9a56a0b14d
Rohit Rai <raistudying@gmail.com>
2023-07-19 12:41:13 +0530

    Click on user photos to view post

--- src/components/user/view/ViewUserPosts.tsx
+++ src/components/user/view/ViewUserPosts.tsx
@@ -1,14 +1,45 @@
+import { useState } from "react";
 import { PostData } from "../../../common/types";
+import ViewUserPost, { ViewUserPostProps } from "./ViewUserPost";
+import { CloseIcon } from "../../../common/icons";
 
 export type ViewUserPostsProps = {
   posts: PostData[];
 };
 
 export default function ViewUserPosts({ posts }: ViewUserPostsProps) {
+  const [viewUserPostProps, setViewUserPostProps] =
+    useState<ViewUserPostProps | null>(null);
+
+  const showUserPost = () => {
+    if (viewUserPostProps) {
+      return (
+        <div className="user-post-dialog">
+          <div className="dialog-background shadow" />
+          <div className="dialog-container">
+            <div
+              className="user-post-close-button primary-container"
+              onClick={() => setViewUserPostProps(null)}
+            >
+              {CloseIcon}
+            </div>
+            <ViewUserPost {...viewUserPostProps} />
+          </div>
+        </div>
+      );
+    }
+  };
+
   const showLeftPhotos = () => {
     return posts.map((post) => {
       if (posts.indexOf(post) % 2 == 0) {
-        return <img key={post.uid} src={post.photoUrl} />;
+        return (
+          <img
+            key={post.uid}
+            src={post.photoUrl}
+            onClick={() => setViewUserPostProps({ post: post })}
+          />
+        );
       }
     });
   };
@@ -16,7 +47,13 @@ export default function ViewUserPosts({ posts }: ViewUserPostsProps) {
   const showRightPhotos = () => {
     return posts.map((post) => {
       if (posts.indexOf(post) % 2 == 1) {
-        return <img key={post.uid} src={post.photoUrl} />;
+        return (
+          <img
+            key={post.uid}
+            src={post.photoUrl}
+            onClick={() => setViewUserPostProps({ post: post })}
+          />
+        );
       }
     });
   };
@@ -27,6 +64,7 @@ export default function ViewUserPosts({ posts }: ViewUserPostsProps) {
         <div className="user-posts-photo-left">{showLeftPhotos()}</div>
         <div className="user-posts-photo-right">{showRightPhotos()}</div>
       </div>
+      {showUserPost()}
     </div>
   );
 }
