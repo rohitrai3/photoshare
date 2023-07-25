a88e11cc1806b1ef8c9897cc944c0e917e1254ca
Rohit Rai <raistudying@gmail.com>
2023-07-25 16:42:49 +0530

    Add photoshare logo

--- src/components/sign_in/SignInHeader.tsx
+++ src/components/sign_in/SignInHeader.tsx
@@ -1,8 +1,12 @@
+import { PhotoShareLogo } from "../../common/icons";
+
 export default function SignInHeader() {
   return (
     <div className="sign-in-header">
       <div className="display-small">Welcome to</div>
-      <div className="display-large">PhotoShare</div>
+      <div className="sign-in-photoshare display-large">
+        {PhotoShareLogo}PhotoShare
+      </div>
     </div>
   );
 }
